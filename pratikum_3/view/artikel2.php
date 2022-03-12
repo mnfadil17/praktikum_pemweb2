b3cfc93e8946d00bf6eae5bcb05ba4e3851eb08b
mnfadil17 <muhammaddil466@gmail.com>
2022-03-12 13:41:33 +0700

    upload tugas

---
+++ pratikum_3/view/artikel2.php
@@ -0,0 +1,39 @@
+<?php
+
+include '../layout/header.php';
+
+?>
+
+<h1 align="center">Welcome To Website </h1>
+    <hr>
+    <p>Ini adalah Artike 2</p>
+    <ul>
+        <li>list</li>
+        <li>list</li>
+        <li>list</li>
+    </ul>
+    <hr>
+    <ol>
+        <li>list</li>
+        <li>list</li>
+        <li>list</li>
+    </ol>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque modi quae dicta,
+        a odio officia sapiente ratione animi, sint illo rerum. Laborum voluptatum voluptas aliquam dicta molestiae
+        recusandae quibusdam iure libero quis a, reprehenderit voluptatem maiores voluptate autem ipsum magnam tenetur
+        culpa sapiente aliquid impedit ad accusantium mollitia nobis sunt. In, et. Voluptate voluptatum cumque at, blanditiis
+        soluta iste sint fuga odio architecto delectus, omnis et. Voluptas perspiciatis aspernatur incidunt dicta culpa natus porro
+        aut facilis! Voluptas distinctio enim recusandae facere, voluptate aspernatur incidunt iure similique id eveniet eaque laudantium
+        assumenda! Recusandae qui quod veritatis ratione est voluptates perspiciatis officiis!</p>
+<br>
+<br>
+<br>
+<br>
+
+
+
+<?php
+include '../layout/footer.php';
+
+
+?>
