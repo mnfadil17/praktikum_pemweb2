994d6de4cf27b071b44f1a1c24096e23bb5797a9
mnfadil17 <muhammaddil466@gmail.com>
2022-03-27 21:31:30 +0700

    upload tugas

---
+++ praktikum_5/ATM/use_akun.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'class_akun.php';
+
+$ac1 = new Account('PGB',10000);
+echo 'sebelum menabung:<br>';
+$ac1->cetak();
+$ac1->deposit(800);
+echo '<br/>setelah menabung 200<br>';
+$ac1->cetak();
+
+$ac2 = new Account('MSI',1000);
+
+?>
